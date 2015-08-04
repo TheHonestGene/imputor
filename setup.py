5bb4aa8c2be409734ad07f65f13dd8c7002a0623
Uemit Seren <uemit.seren@gmail.com>
2015-08-04 10:52:39 +0200

    Initial checking

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='RiskPredictor',
+    version='0.0.1',
+    description='A library for imputation',
+    long_description=long_description,
+    url='https://github.com/TheHonestGene/imputor',
+    author=['Bjarni Vilhjalmsson','Uemit Seren'],
+    author_email='bjarni.vilhjalmsson@gmai.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='Risk Prediction',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=[
+    ],
+    entry_points={
+        'console_scripts': [
+            'inmputor=imputor:main'
+        ],
+    },
+)
+
