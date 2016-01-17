dfa2e8260df83b875d8e9133c801ff610ca75977
Bjarni Vilhjalmsson <bjarni.vilhjalmsson@gmail.com>
2016-01-17 12:42:07 +0100

    Commenting improved, and other minor things

--- imputor/core/impute.py
+++ imputor/core/impute.py
@@ -15,6 +15,8 @@ Kg_nt_decoder = {1:'A', 2:'T', 3:'C', 4:'G', }
 import gzip
 import cPickle
 
+import pylab
+
 cloud_dir = '/Users/bjarnivilhjalmsson/Dropbox/Cloud_folder/'
 repos_dir = '/Users/bjarnivilhjalmsson/REPOS/'
 
@@ -333,41 +335,47 @@ def calc_ld(ref_genotype_file, ld_prefix, window_size = 200, kgenomes_file = 'Da
 
 def impute_23_and_genome(genotype_file=repos_dir+'imputor/tests/data/test_out_genotype.hdf5', 
                          ld_prefix=repos_dir+'imputor/tests/data/ld_dict', window_size = 50,
-                         validation_missing_rate=0.01):
+                         validation_missing_rate=0.01, min_ld_r2_thres = 0.05):
     """
-    
+    validation_missing_rate: The fraction of SNPs used to estimate the imputation accuracy.  1% seems enough.  
+                             A smaller number will increase speed.
+                             
+    min_ld_r2_thres: A minimum LD r2 value for SNPs to be used to impute from.  SNPs with large R2 are more informative for the imputation.
+                     SNPs with r2 values close to 0 are effectively inconsequential for the imputation and can be left out 
+                     (which also speeds up the imputation).  Default is 0.05.
     """
 
     g_h5f = h5py.File(genotype_file,'r')
     
-    #For each SNP
-    #  Identify a (small) window of neighboring SNPs.
-    #  Identify the missing SNPs and non-missing SNPs.
-    #  Invert the matrix, etc.
-    #  Predict genotype
     pred_snps = []
     true_snps = []
     for chrom in range(1,23):
         print 'Working on Chromosome %d'%chrom
 
+        #Loading pre-calculated LD matrices (Note that these could perhaps be stored more efficiently.)
         chrom_str = 'Chr%d'%chrom
         with gzip.open(ld_prefix+'_'+str(window_size)+'_'+chrom_str+'.pickled.gz','r') as f:
             ld_dict = cPickle.load(f)
 
         g_cg = g_h5f[chrom_str]
-        g_sids = g_cg['sids'][...]
-        snps = g_cg['snps'][...]
         
-        imputed_snps = snps.copy()
-
+        #Loading data 
+        snps = g_cg['snps'][...]
         Ds = ld_dict['Ds']
         snp_means = ld_dict['snp_means']
         snp_stds = ld_dict['snp_stds']
+        
+        #The snp vector to be returned
+        imputed_snps = snps.copy()
+
         num_snps = len(snps)
         assert len(Ds)==num_snps,'..bug'
         num_snps_imputed = 0
+        
         for snp_i in range(num_snps):
+            
             if random.random()<validation_missing_rate and snps[snp_i] !=-9:
+                #Picking random SNPs with genotype information to estimate imputation accuracy.
                 true_snp = snps[snp_i]
                 snps[snp_i] = -9
             else:
@@ -377,56 +385,91 @@ def impute_23_and_genome(genotype_file=repos_dir+'imputor/tests/data/test_out_ge
                 #Pull out LD matrix
                 D = Ds[snp_i]
                 
+                #Determining the boundaries of the region.
                 start_i = max(0,snp_i-window_size/2)
                 end_i = min(snp_i+(window_size/2)+1,num_snps)
                 
+                #Obtaining the SNPs in the region, on which the imputation (together with LD) is based.
                 reg_snps = snps[start_i:end_i]
                 reg_snp_means = snp_means[start_i:end_i]
                 reg_snp_means = reg_snp_means.flatten()
                 reg_snp_stds = snp_stds[start_i:end_i]
                 reg_snp_stds = reg_snp_stds.flatten()
                 
+                #The LD vector for the SNP to be imputed.
                 loc_i = snp_i-start_i
                 D_i = D[loc_i] 
 
                 #Filter SNPs that have missing genotypes
                 ok_filter = reg_snps !=-9
-                ld_filter = (D_i**2>0.05)
+                
+                #Filtering SNPs that are not in LD with the SNP to be imputed.  This saves time and may improve accuracy.
+                ld_filter = (D_i**2>min_ld_r2_thres)
                 if sp.any(ok_filter*ld_filter):
                     ok_filter = ok_filter*ld_filter
                 
+                #Filtering the genotypes in the LD region. 
                 assert sp.sum(ok_filter)<len(reg_snps), '..bug'
                 ok_reg_snps = reg_snps[ok_filter]
                 ok_reg_snp_means = reg_snp_means[ok_filter]
                 ok_reg_snp_stds = reg_snp_stds[ok_filter]
                 ok_reg_norm_snps = (ok_reg_snps-ok_reg_snp_means) /ok_reg_snp_stds
                 
+                #Filtering the LD matrix.                 
                 ok_D = (D[ok_filter])[:,ok_filter]
                 ok_D_i = D_i[ok_filter]
-                #Impute genotype.
-            
-                ok_D_inv = linalg.pinv(ok_D)
-                imputed_snp = sp.dot(ok_D_i,sp.dot(ok_D_inv,ok_reg_snps))
                 
-                snp_mean = snp_means[snp_i][0]
-                snp_std = snp_stds[snp_i][0]
-                imputed_snp = imputed_snp*snp_std+snp_mean
-                if imputed_snp<0:
-                    imputed_snp=0
-                elif imputed_snp>2:
-                    imputed_snp=2
+                #Impute genotype.
+                ok_D_inv = linalg.pinv(ok_D)
+                if sp.any(sp.isnan(ok_D_inv)):
+                    print 'Matrix inversion failed!!'
+                    print 'Setting SNP genotype to 1'
+                    imputed_snp=1
+                else:
+                    imputed_snp = sp.dot(ok_D_i,sp.dot(ok_D_inv,ok_reg_snps))
+                    
+                    
+                    #Transform imputed genotype to 0-2 scale
+                    snp_mean = snp_means[snp_i][0]
+                    snp_std = snp_stds[snp_i][0]
+                    imputed_snp = imputed_snp*snp_std+snp_mean
+                    if imputed_snp<0:
+                        imputed_snp=0
+                    elif imputed_snp>2:
+                        imputed_snp=2
+                    
+                #Storing imputed genotypes
                 imputed_snps[snp_i] = imputed_snp
                 if true_snp!=-9:
+                    #Estimate prediction accuracy
                     pred_snps.append(imputed_snp)
                     true_snps.append(true_snp)
                 else:
+                    #Counting the imputed SNPs with actual missing genotype information
                     num_snps_imputed += 1
             
         print imputed_snp
         print 'Number of SNPs imputed so far: %d'%num_snps_imputed
-    print 'Estimated prediction accuracy (R2): %0.4f'%(sp.corrcoef(pred_snps, true_snps)[0,1])**2
+    pred_r2 = (sp.corrcoef(pred_snps, true_snps)[0,1])**2
+    print 'Estimated prediction accuracy (R2): %0.4f'%pred_r2
 
-    return imputed_snps
+    return {'imputed_snps':imputed_snps, 'pred_r2':pred_r2}
+
+def window_size_plot():
+    pred_r2s = []
+    window_sizes = [10,20,30,40,50,60,70,80,90,100]
+    for window_size in window_sizes:
+        calc_ld(repos_dir+'imputor/tests/data/test_out_genotype.hdf5', repos_dir+'imputor/tests/data/ld_dict',window_size=window_size)
+        d = impute_23_and_genome(window_size=window_size)
+        pred_r2s.append(d['pred_r2'])
+    print pred_r2s
+    print window_sizes
+    
+    pylab.plot(window_sizes,pred_r2s,alpha=0.6)
+    pylab.ylabel('Prediction accuracy (R2)')
+    pylab.xlabel('Imputation LD window-size')
+    pylab.savefig(cloud_dir+'tmp/tmp.png')
+
 
     
 #For debugging purposes
@@ -441,8 +484,9 @@ if __name__=='__main__':
 #                          cloud_dir+'tmp/nt_map.pickled',
 #                          repos_dir+'imputor/tests/data/test_out_genotype.hdf5')
 #     
-    window_size = int(sys.argv[1])
-    
-    calc_ld(repos_dir+'imputor/tests/data/test_out_genotype.hdf5', repos_dir+'imputor/tests/data/ld_dict',window_size=window_size)
-    impute_23_and_genome(window_size=window_size)
+#     window_size = int(sys.argv[1])
+#     
+#     calc_ld(repos_dir+'imputor/tests/data/test_out_genotype.hdf5', repos_dir+'imputor/tests/data/ld_dict',window_size=window_size)
+#     impute_23_and_genome(window_size=window_size)
+    window_size_plot()
     
