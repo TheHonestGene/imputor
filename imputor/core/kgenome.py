6b3feda34298971d62cadc3b6a5a0da5e61edee8
Bjarni Jóhan Vihjálmsson <bjv@d17190.local>
2016-01-22 15:51:46 +0100

    working from home

--- imputor/core/kgenome.py
+++ imputor/core/kgenome.py
@@ -6,9 +6,13 @@ opp_strand_dict = {'A':'T', 'G':'C', 'T':'A', 'C':'G'}
 import gzip
 import cPickle
 
+
+cloud_dir = '/Users/bjarnivilhjalmsson/Dropbox/Cloud_folder/'
+repos_dir = '/Users/bjarnivilhjalmsson/REPOS/'
+
     
-def gen_unrelated_eur_1k_data(out_file='/Users/bjarnivilhjalmsson/data/1Kgenomes/1K_genomes_v3_EUR_unrelated.hdf5'):
-    h5f = h5py.File('/Users/bjarnivilhjalmsson/data/1Kgenomes/1K_genomes_v3.hdf5')
+def gen_unrelated_eur_1k_data(out_file='Data/1Kgenomes/1K_genomes_v3_EUR_unrelated2.hdf5'):
+    h5f = h5py.File(cloud_dir+'Data/1Kgenomes/1K_genomes_v3.hdf5')
     eur_filter = h5f['indivs']['continent'][...]=='EUR'
     num_indivs = sp.sum(eur_filter)
     K = sp.zeros((num_indivs,num_indivs), dtype='single')
@@ -27,6 +31,12 @@ def gen_unrelated_eur_1k_data(out_file='/Users/bjarnivilhjalmsson/data/1Kgenomes
         mono_morph_filter = snp_stds>0
         snps = snps[mono_morph_filter]
         snp_stds = snp_stds[mono_morph_filter]
+        print 'Filter SNPs with missing NT information'
+        nts = h5f[chrom_str]['nts'][...]
+        nts = nts[mono_morph_filter]
+        nt_filter = sp.all(nts>0,1)
+        snps = snps[nt_filter]
+        snp_stds = snp_stds[nt_filter]
         print 'Normalizing SNPs'
         snp_means = sp.mean(snps,1)
         norm_snps = (snps - snp_means[sp.newaxis].T)/snp_stds[sp.newaxis].T
@@ -50,12 +60,9 @@ def gen_unrelated_eur_1k_data(out_file='/Users/bjarnivilhjalmsson/data/1Kgenomes
     keep_indivs.sort()
     print 'Retained %d individuals\n'%len(keep_indivs)
 
-    #Map prefix
-    KGenomes_prefix='/Users/bjarnivilhjalmsson/data/1Kgenomes/', 
-    
-    #Filter ambiguous SNPs and store in new file
+    #Store in new file
     print 'Now storing data.'
-    oh5f = h5py.File(out_file,'w')
+    oh5f = h5py.File(cloud_dir+out_file,'w')
     indiv_ids = h5f['indivs']['indiv_ids'][eur_filter]
     indiv_ids = indiv_ids[keep_indivs]
     oh5f.create_dataset('indiv_ids',data =indiv_ids)    
@@ -65,60 +72,49 @@ def gen_unrelated_eur_1k_data(out_file='/Users/bjarnivilhjalmsson/data/1Kgenomes
         snps = h5f[chrom_str]['raw_snps'][...]
         #filter non-europeans.
         snps = snps[:,eur_filter]
+        #Filter related
         snps = snps[:,keep_indivs]
+        #filter monomorphic SNPs
         snp_stds = sp.std(snps,1)
         mono_morph_filter = snp_stds>0
         snps = snps[mono_morph_filter]
-        
-        #Load map and double check..
-        print 'Load  SNP map to get NTs and filter ambiguous SNPs'
-        fn = '%sALL_1000G_phase1integrated_v3_chr%d_impute.legend.gz'%(KGenomes_prefix,chrom_i)
-        ok_sids = set()
-        sids_nt_map = {}
-        with gzip.open(fn) as f:
-            f.next()
-            line_i=0
-            for line in f:
-                l = line.split()
-                nt1=l[2]
-                nt2=l[3]
-                if nt1 not in valid_nts:
-                    continue
-                if nt2 not in valid_nts:
-                    continue
-                if filter_ambiguous and (nt1,nt2) in ambig_nts:
-                    continue
-                line_i +=1
-                ok_sids.add(l[0])
-                sids_nt_map[l[0]]=(nt1,nt2)
-        
-        #FINISH!!!!
-
+        #filter SNPs w missing NT values
+        nts = h5f[chrom_str]['nts'][...]
+        nts = nts[mono_morph_filter]
+        nt_filter = sp.all(nts>0,1)
+        nts = nts[nt_filter]
+        snps = snps[nt_filter]
         
         cg = oh5f.create_group(chrom_str)
-        
         cg.create_dataset('snps',data=snps)
 
         snp_stds = snp_stds[mono_morph_filter]
+        snp_stds = snp_stds[nt_filter]
         snp_means = sp.mean(snps,1)
         cg.create_dataset('snp_means',data=snp_means[sp.newaxis].T)
         cg.create_dataset('snp_stds',data=snp_stds[sp.newaxis].T)
        
         snp_ids = h5f[chrom_str]['snp_ids'][...]
         snp_ids = snp_ids[mono_morph_filter]
+        snp_ids = snp_ids[nt_filter]
         cg.create_dataset('snp_ids',data=snp_ids)
         
         positions = h5f[chrom_str]['positions'][...]
         positions = positions[mono_morph_filter]
+        positions = positions[nt_filter]
         cg.create_dataset('positions',data=positions)
         
         eur_maf = h5f[chrom_str]['eur_maf'][...]
         eur_maf = eur_maf[mono_morph_filter]
+        eur_maf = eur_maf[nt_filter]
         cg.create_dataset('eur_maf',data=eur_maf)
         
-        nts = h5f[chrom_str]['nts'][...]
-        nts = nts[mono_morph_filter]
-        cg.create_dataset('nts',data=nts)
+        decoded_nts = []
+        for nt in nts:
+            nt1 = nt[0]
+            nt2 = nt[1]
+            decoded_nts.append([Kg_nt_decoder[nt1],Kg_nt_decoder[nt2]])
+        cg.create_dataset('nts',data=decoded_nts)
         
         centimorgans = h5f[chrom_str]['centimorgans'][...]
         cg.create_dataset('centimorgans',data=centimorgans)
@@ -129,81 +125,149 @@ def gen_unrelated_eur_1k_data(out_file='/Users/bjarnivilhjalmsson/data/1Kgenomes
     oh5f.close()
     h5f.close()
    
+   
+def gen_1k_test_genotypes(kg_file='Data/1Kgenomes/1K_genomes_v3_EUR_unrelated2.hdf5',nt_map_file=cloud_dir+'tmp/nt_map.pickled', out_prefix=''):
+    """
+    Generates 1K genotypes in the internal genotype format for validation and other purposes.
+    """
+    print 'Loading NT map from file: %s'%nt_map_file
+    f = open(nt_map_file, 'r')
+    snp_map_dict = cPickle.load(f)
+    f.close()
+    
+    h5f = h5py.File(kg_file)
 
+    
+    for indiv in kg_indivs:
+        print 'Parsing genotypes: %s'%h5file
+        h5f = h5py.File(h5file,'r')
+        chromosomes = range(1,23) 
+        
+        #prepare output file
+        oh5f = h5py.File(out_h5file)
+        
+        tot_num_parsed_snps = 0
+        for chrom in chromosomes:
+            print '\nWorking on chromosome %d'%chrom
+            kg_chrom_str = 'chr%d'%chrom
+            chrom_str = 'Chr%d'%chrom
+            cg = h5f[chrom_str]
+            sids = cg['ids'][...]
+            raw_snps = cg['snps'][...]
+            
+            #Get the nucleotides coding map (from 1K genomes project).
+            chrom_dict = snp_map_dict[kg_chrom_str]
+            sid_nt_map = chrom_dict['sid_nt_map']
+            n = len(sid_nt_map)
+            snps = sp.repeat(-9, n) #Creating the SNP with fixed size 
+            num_not_found = 0
+            num_misunderstood = 0
+            num_parsed_ok = 0
+            for sid, nt in izip(sids,raw_snps):
+                try:
+                    d = sid_nt_map[sid]
+                except Exception:
+                    num_not_found +=1
+                    continue
+                try:
+                    nt_val = d['ntm'][nt]
+                except Exception:
+                    num_misunderstood +=1
+                    continue
+                snps[d['snp_i']] = nt_val
+                num_parsed_ok += 1
+                
+            print "%d SNPs weren't found and %d SNPs had unrecognizable nucleotides"%(num_not_found,num_misunderstood) 
+            print "%d SNPs were parsed ok."%num_parsed_ok
+            tot_num_parsed_snps +=num_parsed_ok
+            #Not sure what information we need, perhaps only the SNPs?
+            
+            assert len(snps)==len(chrom_dict['sids'])==len(chrom_dict['positions'])==len(chrom_dict['nts']), '..bug'
+            #Store information
+            cg = oh5f.create_group(chrom_str)
+            cg.create_dataset('snps', data=snps)
+            cg.create_dataset('sids', data=chrom_dict['sids'])
+            cg.create_dataset('positions', data=chrom_dict['positions'])
+            cg.create_dataset('nts', data=chrom_dict['nts'])
+            #Also positions..
+            
+            #genome_dict[chrom]={'snps':snps, } #'sids':chrom_dict['sids'], 'positions':chrom_dict['positions'], 'nts':chrom_dict['nts']}
+        print 'In total %d SNPs were parsed.'%tot_num_parsed_snps
+        h5f.close()
+        oh5f.close()
+        #return genome_dict        
 
 
 
 #Coding key
-def prepare_nt_coding_key(K_genomes_snps_map, indiv_genot_file, nt_map_file):
-    """
-    Determines the nucleotide coding for the genotype using the 1K genome  the 10KUK
-    """
-    gf = h5py.File(indiv_genot_file,'r')
-    kgf = h5py.File(K_genomes_snps_map,'r')
-    chromosomes = range(1,23) 
-    snp_map_dict = {}
-    num_snps = 0
-    for chrom in chromosomes:
-        print 'Working on chromosome %d'%chrom
-        kg_chrom_str = 'chr%d'%chrom
-        chrom_str = 'Chr%d'%chrom
-        
-        #Get SNPs from genotype
-        cg = gf[chrom_str]
-        sids = cg['ids'][...]
-        snps = cg['snps'][...]
-        sid_dict = dict(zip(sids, snps))
-        
-        #Get SNP IDs from 1K genomes
-        kcg = kgf[kg_chrom_str]
-        kg_sids = kcg['snp_ids'][...]
-        
-        #Determine overlap between SNPs..
-        kg_filter = sp.in1d(kg_sids,sids)
-        kg_sids = kg_sids[kg_filter]
-        kg_nts = (kcg['nts'][...])[kg_filter]
-        kg_positions = (kcg['positions'][...])[kg_filter]
-        
-        #Check that nt are ok in genotype data, otherwise filter.
-        sid_nt_map = {}
-        positions = []
-        ok_sids = []
-        nts = []
-        snp_i = 0
-        for sid, kg_nt, kg_pos in izip(kg_sids, kg_nts, kg_positions):
-            snp = sid_dict[sid]
-            if tuple(kg_nt) not in ambig_nts:
-                # All possible (and allowed) nucleotides strings 
-                ntm = {}
-                ntm['--']=-9
-                ntm['-'+kg_nt[0]]=-9
-                ntm['-'+kg_nt[1]]=-9
-                ntm[kg_nt[0]+'-']=-9
-                ntm[kg_nt[1]+'-']=-9
-                ntm[kg_nt[0]+kg_nt[0]]=0
-                ntm[kg_nt[1]+kg_nt[0]]=1
-                ntm[kg_nt[0]+kg_nt[1]]=1
-                ntm[kg_nt[1]+kg_nt[1]]=2
-                sid_nt_map[sid]={'ntm':ntm, 'snp_i':snp_i}
-                positions.append(kg_pos)
-                nts.append(kg_nt)
-                ok_sids.append(sid)
-                snp_i += 1
-        
-        num_snps += len(sid_nt_map)
-        snp_map_dict[kg_chrom_str]={'sid_nt_map':sid_nt_map, 'positions':positions, 'nts':nts, 'sids':ok_sids}
-    
-    print 'Found %d SNPs'%num_snps
-    print 'Writing to file'
-    f = open(nt_map_file, 'wb')
-    cPickle.dump(snp_map_dict, f, protocol=2)
-    f.close()
-    return snp_map_dict
+# def prepare_nt_coding_key(K_genomes_snps_map, indiv_genot_file, nt_map_file):
+#     """
+#     Determines the nucleotide coding for the genotype using the 1K genome  the 10KUK
+#     """
+#     gf = h5py.File(indiv_genot_file,'r')
+#     kgf = h5py.File(K_genomes_snps_map,'r')
+#     chromosomes = range(1,23) 
+#     snp_map_dict = {}
+#     num_snps = 0
+#     for chrom in chromosomes:
+#         print 'Working on chromosome %d'%chrom
+#         kg_chrom_str = 'chr%d'%chrom
+#         chrom_str = 'Chr%d'%chrom
+#         
+#         #Get SNPs from genotype
+#         cg = gf[chrom_str]
+#         sids = cg['ids'][...]
+#         snps = cg['snps'][...]
+#         sid_dict = dict(zip(sids, snps))
+#         
+#         #Get SNP IDs from 1K genomes
+#         kcg = kgf[kg_chrom_str]
+#         kg_sids = kcg['snp_ids'][...]
+#         
+#         #Determine overlap between SNPs..
+#         kg_filter = sp.in1d(kg_sids,sids)
+#         kg_sids = kg_sids[kg_filter]
+#         kg_nts = (kcg['nts'][...])[kg_filter]
+#         kg_positions = (kcg['positions'][...])[kg_filter]
+#         
+#         #Check that nt are ok in genotype data, otherwise filter.
+#         sid_nt_map = {}
+#         positions = []
+#         ok_sids = []
+#         nts = []
+#         snp_i = 0
+#         for sid, kg_nt, kg_pos in izip(kg_sids, kg_nts, kg_positions):
+#             snp = sid_dict[sid]
+#             if tuple(kg_nt) not in ambig_nts:
+#                 # All possible (and allowed) nucleotides strings 
+#                 ntm = {}
+#                 ntm['--']=-9
+#                 ntm['-'+kg_nt[0]]=-9
+#                 ntm['-'+kg_nt[1]]=-9
+#                 ntm[kg_nt[0]+'-']=-9
+#                 ntm[kg_nt[1]+'-']=-9
+#                 ntm[kg_nt[0]+kg_nt[0]]=0
+#                 ntm[kg_nt[1]+kg_nt[0]]=1
+#                 ntm[kg_nt[0]+kg_nt[1]]=1
+#                 ntm[kg_nt[1]+kg_nt[1]]=2
+#                 sid_nt_map[sid]={'ntm':ntm, 'snp_i':snp_i}
+#                 positions.append(kg_pos)
+#                 nts.append(kg_nt)
+#                 ok_sids.append(sid)
+#                 snp_i += 1
+#         
+#         num_snps += len(sid_nt_map)
+#         snp_map_dict[kg_chrom_str]={'sid_nt_map':sid_nt_map, 'positions':positions, 'nts':nts, 'sids':ok_sids}
+#     
+#     print 'Found %d SNPs'%num_snps
+#     print 'Writing to file'
+#     f = open(nt_map_file, 'wb')
+#     cPickle.dump(snp_map_dict, f, protocol=2)
+#     f.close()
+#     return snp_map_dict
         
 #For debugging purposes
 if __name__=='__main__':        
-    prepare_nt_coding_key('/Users/bjarnivilhjalmsson/data/1Kgenomes/1K_genomes_v3_EUR_unrelated.hdf5',
-                          '/Users/bjarnivilhjalmsson/REPOS/imputor/tests/data/test_genotype.hdf5',
-                          '/Users/bjarnivilhjalmsson/data/tmp/nt_map2.pickled')
+    pass
 
 
