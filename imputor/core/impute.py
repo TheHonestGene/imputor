a61c7f61b14dc58c757e8fe7cb6969996581674d
Bjarni Jóhan Vihjálmsson <bjarni.vilhjalmsson@gmail.com>
2016-02-03 15:46:43 +0100

    uemits parser

--- imputor/core/impute.py
+++ imputor/core/impute.py
@@ -19,7 +19,39 @@ import pylab
 cloud_dir = '/Users/bjv/Dropbox/Cloud_folder/'
 repos_dir = '/Users/bjv/REPOS/'
 
-
+def convert_to_hdf5(file_name,out_file_name):
+    #Covert 23andme file to a HDF5 file.
+    with open(file_name,'r') as f:
+        data = f.readlines()
+    csv_content = data.decode("utf-8")
+    start_chr = None
+    f = h5py.File('%s.hdf5' % genotype_id)
+    pos_snps =[]
+    
+    for row in csv_content.splitlines():
+        if len(row) == 0 or row[0] == '#':
+            continue
+        cols = row.split("\t")
+        if len(cols) != 4:
+            continue
+        if cols[1] != start_chr:
+            if start_chr is not None:
+                sorted(pos_snps, key=lambda x: x[0])
+                positions,ids,snps = zip(*pos_snps)
+                snpids_dset = group.create_dataset('ids',(len(positions),),chunks=True,compression='lzf',dtype='S15',data=ids)
+                pos_dset = group.create_dataset('positions',(len(positions),),chunks=True,compression='lzf',dtype='i8',data=positions)
+                snp_dset = group.create_dataset('snps',(len(positions),),chunks=True,compression='lzf',dtype='S2',data=snps)
+                pos_snps =[]
+            start_chr = cols[1]
+            group = f.create_group("Chr%s" % start_chr)
+        pos_snps.append((int(cols[2]),cols[0].encode('utf-8'),cols[3].encode('utf-8')))
+    sorted(pos_snps, key=lambda x: x[0])
+    positions,ids,snps = zip(*pos_snps)
+    snpids_dset = group.create_dataset('ids',(len(positions),),chunks=True,compression='lzf',dtype='S15',data=ids)
+    pos_dset = group.create_dataset('positions',(len(positions),),chunks=True,compression='lzf',dtype='i8',data=positions)
+    snp_dset = group.create_dataset('snps',(len(positions),),chunks=True,compression='lzf',dtype='S2',data=snps)
+    
+        
 #Coding key
 def prepare_nt_coding_key(K_genomes_snps_map, indiv_genot_file, nt_map_file):
     """
