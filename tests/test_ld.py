f2eaa64fa3d472d47639c4807e066dc5cbecacc1
Uemit Seren <uemit.seren@gmail.com>
2017-11-05 22:14:46 +0100

    Added support for testing
    Added py.test teststubs and also configuraiton for .travis and tox. The test will be executed in both py27 and py36 environments. Currently all tests fail as not implemented.
    Also all necessary fixtures are defined. The actual fixture data must be created

---
+++ tests/test_ld.py
@@ -0,0 +1,8 @@
+import pytest
+from imputor.core import impute
+
+class TestNtCodingKey:
+
+    def test_calculate_ld(self, pop_dataset_prepared, ld_folder, nt_map, tmpdir):
+        # TODO call impute and test ld files are correctly created
+        raise Exception('Test not implemented')
---
+++ tests/test_nt_coding_key.py
@@ -0,0 +1,8 @@
+import pytest
+from imputor.core import nt_coding_key
+
+class TestNtCodingKey:
+
+    def test_create_nt_coding_key_map(self, pop_dataset, genotype_as_hdf5, nt_map, tmpdir):
+        # TODO call nt_coding_key and test nt map is correctly created (compare with nt_map)
+        raise Exception('Test not implemented')
